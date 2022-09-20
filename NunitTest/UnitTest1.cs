d634d985ff4984978630a6fe3f124396b21d7798
Alec Harrison <aah1@mac.com>
2022-09-20 16:44:45 -0500

    First check in

---
+++ NunitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Novice_to_dangerous;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
